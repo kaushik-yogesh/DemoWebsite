f165103f2bb59af734811b9d4e1fc086a8f69520
Yogesh Kaushik <Yogeshkaushik138@gmail.com>
2025-11-01 21:47:59 +0530

    Updated something

--- DemoWebsite/src/App.jsx
+++ DemoWebsite/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
+      <h1>Yogesh Kaushik</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
